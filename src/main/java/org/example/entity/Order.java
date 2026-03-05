5e4b9835df9344560553088e4f9c70b7e936b4a0
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-03-05 17:14:14 +0100

    add total() function

--- src/main/java/org/example/entity/Order.java
+++ src/main/java/org/example/entity/Order.java
@@ -1,9 +1,16 @@
 package org.example.entity;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public record Order(
         int id,
         List<Product> products
 ) {
+
+    public BigDecimal total(){
+        return products.stream()
+                .map(Product::price)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 }
