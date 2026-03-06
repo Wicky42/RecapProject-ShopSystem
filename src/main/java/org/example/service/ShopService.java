52f0ea7d57879575ddac3621e9b825d905ba313d
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-03-06 11:51:41 +0100

    update newOrder function. Availabiliy is reduced for every orderd product

--- src/main/java/org/example/service/ShopService.java
+++ src/main/java/org/example/service/ShopService.java
@@ -13,21 +13,28 @@ public class ShopService {
     private final ProductRepo productRepo;
     private final OrderRepoInterface orderRepo;
 
-    private final int nextOrderId = 12345;
+    private static int nextOrderId = 12345;
 
     public ShopService(ProductRepo productRepo, OrderRepoInterface orderListRepo) {
         this.productRepo = productRepo;
         this.orderRepo = orderListRepo;
     }
 
-    public Order newOrder(Integer ...ids){
+    public Order newOrder(Integer... ids) {
         List<Product> products = new ArrayList<>();
-        for(Integer id : ids){
-            Product toAdd = productRepo.findById(id)
-                    .orElseThrow(()->new IllegalArgumentException("Product not found: " + id));
-            products.add(toAdd);
+
+        for (Integer id : ids) {
+            Product product = productRepo.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("Product not found: " + id));
+            products.add(product);
         }
-        Order newOrder = new Order(nextOrderId+1, products);
+
+        for (Product product : products) {
+            Product updatedProduct = product.sell(1);
+            productRepo.update(updatedProduct);
+        }
+
+        Order newOrder = new Order(nextOrderId++, products);
         orderRepo.add(newOrder);
         return newOrder;
     }
