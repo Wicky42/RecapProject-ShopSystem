390ee38d4ceb1d885b88882c65aed0ad3432840b
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-03-06 11:50:15 +0100

    add a sell function

--- src/main/java/org/example/entity/Product.java
+++ src/main/java/org/example/entity/Product.java
@@ -7,4 +7,11 @@ public record Product(
         String name,
         BigDecimal price,
         int availablitity
-) { }
+) {
+    public Product sell(int amount){
+        if(amount > availablitity){
+            throw new IllegalArgumentException("Not enough in stock");
+        }
+        return new Product(id, name, price, (availablitity-amount));
+    }
+}
