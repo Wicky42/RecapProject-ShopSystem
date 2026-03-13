b222c25ad6fd8485548188d92027115a418bbae8
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-03-13 11:32:50 +0100

    ship() & complete() order

--- src/main/java/org/example/entity/Order.java
+++ src/main/java/org/example/entity/Order.java
@@ -14,4 +14,18 @@ public record Order(
                 .map(OrderItem::subtotal)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
+
+    public Order ship(){
+        if( orderStatus != OrderStatus.PROCESSING){
+            throw new IllegalStateException("Nur Bestellungen in Verarbeitung können verschickt werden");
+        }
+        return new Order(id, items, OrderStatus.IN_DELIVERY);
+    }
+
+    public Order complete(){
+        if( orderStatus != OrderStatus.IN_DELIVERY){
+            throw new IllegalStateException("Nur Bestellungen in Zusellung können abgeschlossen werden");
+        }
+        return new Order(id, items, OrderStatus.COMPLETED);
+    }
 }
